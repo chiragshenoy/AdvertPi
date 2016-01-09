a2da02918b797c3796629a82a7ee646364714ef3
chiragshenoy <chiragshenoy@gmail.com>
2016-01-10 00:52:46 +0530

    before making collapsible

--- app/src/main/java/com/example/chirag/under25/MainActivity.java
+++ app/src/main/java/com/example/chirag/under25/MainActivity.java
@@ -1,65 +1,80 @@
 package com.example.chirag.under25;
 
-import android.app.DownloadManager;
-import android.net.http.RequestQueue;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.apache.http.HttpEntity;
+import com.squareup.picasso.Picasso;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+import it.carlom.stikkyheader.core.StikkyHeaderBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
-    JSONObject json;
-    HttpClient client;
-    //       String URL = "http://10.10.0.1:80/a.php";
-//    String URL = "https://api.myjson.com/bins/1uln5";
-//    String URL = "http://115.242.173.133:8080/inheritance.php";
-    String URL = "http://abcd.com";
-    String ssid;
-    String key;
+
+    RecyclerView mRecyclerView;
+    ArrayList<Item> itemArrayList;
+    TextView tag;
+    String sTag;
+    MyAdapter mAdapter;
+    String imageURL = "";
+    ImageView header_image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) findViewById(R.id.textView);
-
-        ssid = "Test";
-        key = "0000000000";
-
-        client = new DefaultHttpClient();
+        header_image = (ImageView) findViewById(R.id.header_image);
 
 
-        initWifi();
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        itemArrayList = new ArrayList<Item>();
+        tag = (TextView) findViewById(R.id.tag);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setLayoutManager(layoutManager);
 
 
-    }
+        String ssid = "Test";
+        String key = "0000000000";
 
-    private void initWifi() {
         WifiConfiguration wifiConfig = new WifiConfiguration();
         wifiConfig.SSID = String.format("\"%s\"", ssid);
         wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-
         WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-
+        //remember id
         int netId = wifiManager.addNetwork(wifiConfig);
         wifiManager.disconnect();
         wifiManager.enableNetwork(netId, true);
@@ -67,29 +82,97 @@ public class MainActivity extends AppCompatActivity {
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
+            @Override
             public void run() {
-                new Read().execute();
+                new con().execute();
             }
         }, 3000);
 
+        mAdapter = new MyAdapter(getApplication(), itemArrayList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        FrameLayout f = (FrameLayout) findViewById(R.id.layout_container);
+
+        IO2014HeaderAnimator animator = new IO2014HeaderAnimator(getApplicationContext());
+
+        StikkyHeaderBuilder.stickTo(mRecyclerView)
+                .setHeader(R.id.header, f)
+                .animator(animator)
+                .minHeightHeader(250)
+                .build();
+
     }
 
+    public class con extends AsyncTask<String, Void, String> {
 
-    public class Read extends AsyncTask<String, Integer, String> {
-        /**
-         * progress dialog to show user that the backup is processing.
-         */
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            mAdapter.notifyDataSetChanged();
+            Picasso.with(getApplicationContext())
+                    .load(imageURL)
+                    .resize(500, 150)
+                    .into(header_image);
 
-        //Only getting the list of the subjects and getting basic info
+            tag.setText(sTag);
+        }
+
         @Override
         protected String doInBackground(String... params) {
-            try {
-                json = get_entire_json();
-                String string_basic_info = json.getString("offers");
-                Log.e("offers", string_basic_info);
-                return null;
+            HttpClient client = new DefaultHttpClient();
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+            HttpResponse response;
 
-            } catch (ClientProtocolException e) {
+            String serverResponse = null;
+            try {
+
+                HttpGet get = new HttpGet("http://abcd.com/");
+
+
+                response = client.execute(get);
+//*
+                if (response != null) {
+                    InputStream in = response.getEntity().getContent(); //Get the data in the entity
+                    serverResponse = convertStreamToString(in);
+                    Log.d("serverResponse", serverResponse);
+
+
+                    JSONObject jObj = new JSONObject(serverResponse);
+                    sTag = jObj.getString("name");
+                    imageURL = jObj.getString("logo");
+                    Log.d("name", sTag);
+                    JSONArray arr = jObj.getJSONArray("array");
+
+                    for (int i = 0; i < arr.length(); i++) {
+                        JSONObject arJ = arr.getJSONObject(i);
+//                        String im = arJ.getString("image");
+//                        String head = arJ.getString("heading");
+//                        String desc = arJ.getString("description");
+//                        String oldPrice = arJ.getString("oldprice");
+//                        String newPrice = arJ.getString("newprice");
+                        Item item = new Item();
+
+                        item.setDescription(arJ.getString("description"));
+                        item.setHeader(arJ.getString("heading"));
+                        item.setNew_price(arJ.getString("newprice"));
+                        item.setOld_price(arJ.getString("oldprice"));
+                        item.setImage(arJ.getString("image"));
+                        itemArrayList.add(item);
+
+//                        Log.d("ARRAY ", "" + im + "\n" + head + "\n" + desc);
+                    }
+                    JSONObject jo = jObj.getJSONObject("trending");
+
+                    String image = jo.getString("image");
+                    String description = jo.getString("description");
+                    String price = jo.getString("price");
+                    String discount = jo.getString("discount");
+
+                    Log.d("TRENDING ", "" + image + "\n" + description + "\n" + price + "\n" + discount);
+
+
+                }
+            } catch (ClientProtocolException | UnsupportedEncodingException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -97,44 +180,31 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
+            return serverResponse;
 
         }
 
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
+        private String convertStreamToString(InputStream is) {
 
-        }
-    }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
 
-    public JSONObject get_entire_json() throws IOException, JSONException {
-
-//        StringBuilder url = new StringBuilder(URL + email + PASSWORD);
-        StringBuilder url = new StringBuilder(URL);
-        HttpGet get = new HttpGet(url.toString());
-        HttpResponse r = client.execute(get);
-
-        int status = r.getStatusLine().getStatusCode();
-
-        if (status == 200) {
-            HttpEntity e = r.getEntity();
-            String data = EntityUtils.toString(e);
-            JSONObject full_json = new JSONObject(data);
-
-            return full_json;
-        } else {
-            Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
-            return null;
-
+            String line = null;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append((line + "\n"));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return sb.toString();
         }
     }
-
 
 }
